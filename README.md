4a63e5b4590217e656f9c62b5ca14edf06aaf8ec
Kath <kcasaey411@icloud.com>
2024-11-02 11:52:10 -0400

    Initializing repository

---
+++ README.md
@@ -0,0 +1,5 @@
+this code needs to be able to:
+- access the excel file
+- save it as an image
+- update the background image of the laptop (https://www.youtube.com/watch?v=yd6qH4EJFX4)
+- either continuously run without opening a terminal OR run by the click of a shortcut
---
+++ tasklist_updater.py
@@ -0,0 +1,4 @@
+# this code will check an excel file and save that file as a pdf/png
+# then it will update the background of the computer to have that image
+# the code will either check the file when it is run due to a shortcut on the
+# homescreen or it will check every few hours
